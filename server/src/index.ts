d23223dcf26d0ec73fab31671d2687a192070260
Robert Gravina <robert.gravina@gmail.com>
2020-03-05 13:58:35 +0900

    Use Typescript with node.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,17 +4,17 @@ const path = require('path');
 const app = express();
 
 // Location of the React HTML/JS/CSS etc.
-app.use(express.static(path.join(__dirname, '/../../client/dist')));
+app.use(express.static(path.join(__dirname, '/../../../client/dist')));
 
 // Example API endpoint
-app.get('/api/almonds', (req,res) => {
+app.get('/api/almonds', (request: any, response: any) => {
     var list = [ "honey butter", "hot and spicy chicken", "pang pang",  "tiramisu", "tteokbokki", "wasabi"];
-    res.json(list);
+    response.json(list);
 });
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/../../client/dist/index.html'));
+// Serve the React app if any other url
+app.get('*', (request: any, response: any) =>{
+    response.sendFile(path.join(__dirname+'/../../../client/dist/index.html'));
 });
 
 const port = process.env.PORT || 8000;
